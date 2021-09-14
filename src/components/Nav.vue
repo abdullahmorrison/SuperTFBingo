f278c4d1c06e8a00a3dbb4fce5e29ba0ae8b0e7e
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-09-14 13:48:30 -0400

    completed nav stylng

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <nav>
+    <img src="../assets/logo.svg" alt="SuprTF Emote">
+    <h1>SuperTF Bingo</h1>
+  </nav>
 </template>
 
 <script>
@@ -8,6 +11,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  nav{
+    background: #fc4f03;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+  }
+  nav h1{
+    color: white;
+    font-size: xx-large;
+  }
+  nav img{
+    margin-right: 10px;
+  }
 </style>
