efb78e785908e253a5f46c0ed06d8ff01aa80b4d
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-09-14 13:35:30 -0400

    completed footer styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <footer>
+    <a href="mailto:abdullahmorrison@gmail.com" class="link" target="__blank">
+      <img src="../assets/footer/emailIcon.svg" alt="Email Icon">
+      Contact Dev
+    </a>
+    <a href="https://github.com/abdullahmorrison" class="link" target="__blank">
+      <img src="../assets/footer/gitHubLogo.svg" alt="GitHub Logo">
+      View Git Repo
+    </a>
+    <a href="https://ko-fi.com/abdullahmorrison" class="link" target="__blank">
+      <img src="../assets/footer/coffeeIcon.svg" alt="Coffee Icon">
+      Buy Me a Coffee :)
+    </a>
+  </footer>
 </template>
 
 <script>
@@ -8,6 +21,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  footer{
+    background: white;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+  }
+  .link{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
 
+    margin: 0 20px;
+  }
+  img{
+    width: 35px
+  }
 </style>
