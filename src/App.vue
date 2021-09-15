151c00adaccffa9b965e758dddf379b18773d2b5
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-09-15 12:13:09 -0400

    added components to app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav/>
+  <main class="game">
+    <Description/>
+    <Board/>
+  </main>
+  <Footer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+import Description from './components/Description.vue'
+import Board from './components/Board.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Description,
+    Board,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  padding: 0;
+  margin: 0;
+}
+body{
+  background:#a0a7aa;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.game{
+  min-height: 90vh;
+  display: flex;
+  justify-content: center;
+    flex-wrap: wrap;
 }
 </style>
