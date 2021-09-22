0e1c3c6313214291f4a896e020ccad2bbd432d72
Abdullah Morrison <amorr128@uottawa.ca>
2021-09-22 11:13:53 -0400

    added comment to main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueGtag from "vue-gtag-next";
+import VueGtag from "vue-gtag-next";//for google analytics
 
 const app = createApp(App);
 
+//for google analytics
 app.use(VueGtag, {
   property: {
     id: "G-D3VY41X17M"
