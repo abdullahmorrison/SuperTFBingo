c9ac5a6d0bf4ffb48b4bf090b0cc1b56f63db667
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-10-16 17:56:52 -0400

    created difficulty selection component

---
+++ src/components/DifficultySelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <select name="Difficulty" @change="$emit('selectDifficulty', $event.target.value)">
+      <option value="Easy">Easy</option>
+      <option value="Impossible">Impossible</option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'DifficultySelect'
+}
+</script>
+
+<style scoped>
+select{
+    box-shadow: 0 3px 2px #6f6f6f;
+    font-size: large;
+    padding: 10px;
+}
+select:hover{
+  cursor: pointer;
+}
+</style>
