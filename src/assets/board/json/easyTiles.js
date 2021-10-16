14ef575ea80a340e74637c42c1c0230f46635726
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-10-16 14:04:53 -0400

    moved tile data from the board to a seperate js file

---
+++ src/assets/board/json/easyTiles.js
@@ -0,0 +1,182 @@
+import amongS from '../images/easyTiles/amongS.svg'
+import TwoByTwo from '../images/easyTiles/2by2.svg'
+import MODS from '../images/easyTiles/MODS.svg'
+import baboon from '../images/easyTiles/baboon.svg'
+import sJerk from '../images/easyTiles/sJerk.svg'
+
+import COPIUM from '../images/easyTiles/COPIUM.svg'
+import Bedge from '../images/easyTiles/Bedge.svg'
+import PepeW from '../images/easyTiles/PepeW.svg'
+import NOOO from '../images/easyTiles/NOOO.svg'
+import DANKIES from '../images/easyTiles/DANKIES.svg'
+
+import suprJOY from '../images/easyTiles/suprJOY.svg'
+import PepegaAim from '../images/easyTiles/PepegaAim.svg'
+import discordHoodie from '../images/easyTiles/discordHoodie.svg'
+import ChugU from '../images/easyTiles/ChugU.svg'
+import VeryCool from '../images/easyTiles/VeryCool.svg'
+
+import suprOMEGALUL from '../images/easyTiles/suprOMEGALUL.svg'
+import FourWeirdBuff from '../images/easyTiles/4WeirdBuff.svg'
+import MYEYES from '../images/easyTiles/MYEYES.svg'
+import pepeCD from '../images/easyTiles/pepeCD.svg'
+import peePoo from '../images/easyTiles/peePoo.svg'
+
+import borpaSpin from '../images/easyTiles/borpaSpin.svg'
+import MTD from '../images/easyTiles/MTD.svg'
+import suprWTF from '../images/easyTiles/suprWTF.svg'
+import FourGitte from '../images/easyTiles/4Gitte.svg'
+import Wokeage from '../images/easyTiles/Wokeage.svg'
+
+export default [
+    {
+        "clicked": false,
+        "img": amongS,
+        "alt": "amongS Emote",
+        "title": "Among Us Reference"
+    },
+    {
+        clicked: false,
+        img: TwoByTwo,
+        alt: "Super 2x2",
+        title: "2x2 in Chat"
+    },
+    {
+        clicked: false,
+        img: MODS,
+        alt: "MODS Emote",
+        title: "\"Shoot That Guy\""
+    },
+    {
+        clicked: false,
+        img: baboon,
+        alt: "Image of a Baboon",
+        title: "Calls Someone a Baboon"
+    },
+    {
+        clicked: false,
+        img: sJerk,
+        alt: "sJerk Emote",
+        title: "Jerking Self Off"
+    },
+    {
+        clicked: false,
+        img: COPIUM,
+        alt: "COPIUM Emote",
+        title: "Coping"
+    },
+    {
+        clicked: false,
+        img: Bedge,
+        alt: "Bedge Emote",
+        title: "Orisa Gameplay"
+    },
+    {
+        clicked: false,
+        img: PepeW,
+        alt: "PepeW Emote",
+        title: "Volskaya"
+    },
+    {
+        clicked: false,
+        img: NOOO,
+        alt: "NOOO Emote",
+        title: "\"NOOOOO\""
+    },
+    {
+        clicked: false,
+        img: DANKIES,
+        alt: "DANKIES Emote",
+        title: "Mumbling Words"
+    },
+    {
+        clicked: false,
+        img: suprJOY,
+        alt: "suprJOY Emote",
+        title: "\"Just Kidding Suck My Balls\""
+    },
+    {
+        clicked: false,
+        img: PepegaAim,
+        alt: "PepegaAim Emote",
+        title: "Trash Aim"
+    },
+    {
+        clicked: false,
+        img: discordHoodie,
+        alt: "Discord Hoodie",
+        title: "Wearing Discord Hoodie"
+    },
+    {
+        clicked: false,
+        img: ChugU,
+        alt: "ChugU Emote",
+        title: "New Chug"
+    },
+    {
+        clicked: false,
+        img: VeryCool,
+        alt: "VeryCool Emote",
+        title: "Eating On Stream"
+    },
+    {
+        clicked: false,
+        img: suprOMEGALUL,
+        alt: "suprOMEGALUL Emote",
+        title: "Good One Dono"
+    },
+    {
+        clicked: false,
+        img: FourWeirdBuff,
+        alt: "4WeirdBuff Emote",
+        title: "\"DAAAPHNEE\""
+    },
+    {
+        clicked: false,
+        img: MYEYES,
+        alt: "MYEYES Emote",
+        title: "Wiffed Shatter"
+    },
+    {
+        clicked: false,
+        img: pepeCD,
+        alt: "pepeCD Emote",
+        title: "Doc Song Playing"
+    },
+    {
+        clicked: false,
+        img: peePoo,
+        alt: "peePoo Emote",
+        title: "Bathroom Break"
+    },
+    {
+        clicked: false,
+        img: borpaSpin,
+        alt: "borpaSpin Emote",
+        title: "\"Cum\""
+    },
+    {
+        clicked: false,
+        img: MTD,
+        alt: "MTD Emote",
+        title: "Main Tank Difference"
+    },
+    {
+        clicked: false,
+        img: suprWTF,
+        alt: "suprWTF Emote",
+        title: "WTF Dono"
+    },
+    {
+        clicked: false,
+        img: FourGitte,
+        alt: "4Gitte Emote",
+        title: "Unstoppable Brig"
+    },
+    {
+        clicked: false,
+        img: Wokeage,
+        alt: "Wokeage Emote",
+        title: "Variety Time"
+    }
+]
