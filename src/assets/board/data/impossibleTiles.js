416e0722b56b63b278c301b57aaeb43d9f401365
Abdullah Morrison <abdullahmorrison@gmail.com>
2021-10-16 16:50:16 -0400

    created tilles of impossible difficulty

---
+++ src/assets/board/data/impossibleTiles.js
@@ -0,0 +1,177 @@
+import dmcaLogo from '../images/impossibleTiles/dmcaLogo.svg'
+import clbCover from '../images/impossibleTiles/clbCover.svg'
+import fourWeirdGym from '../images/impossibleTiles/fourWeirdGym.svg'
+import goldenKappa from '../images/impossibleTiles/goldenKappa.svg'
+import viewerIcon from '../images/impossibleTiles/viewerIcon.svg'
+import owTrophies from '../images/impossibleTiles/owTrophies.svg'
+import owRank from '../images/impossibleTiles/owRank.svg'
+import reinImage from '../images/impossibleTiles/reinImage.svg'
+import superCloseUp from '../images/impossibleTiles/superCloseUp.svg'
+import genshinCover from '../images/impossibleTiles/genshinCover.svg'
+import superHairCut from '../images/impossibleTiles/superHairCut.svg'
+import ow2Logo from '../images/impossibleTiles/ow2Logo.svg'
+import tifa from '../images/impossibleTiles/tifa.svg'
+import superCashew from '../images/impossibleTiles/superCashew.svg'
+import sevenTV from '../images/impossibleTiles/sevenTV.svg'
+import DIESOFCRINGE from '../images/impossibleTiles/DIESOFCRINGE.svg'
+import fran from '../images/impossibleTiles/fran.svg'
+import superBeard from '../images/impossibleTiles/superBeard.svg'
+import superTier3 from '../images/impossibleTiles/superTier3.svg'
+import doomfist from '../images/impossibleTiles/doomfist.svg'
+import NaM from '../images/impossibleTiles/NaM.svg'
+import persona5 from '../images/impossibleTiles/persona5.svg'
+import owQueue from '../images/impossibleTiles/owQueue.svg'
+import suprL from '../images/impossibleTiles/suprL.svg'
+
+export default [
+    {
+        "clicked": false,
+        "img": dmcaLogo,
+        "alt": "DMCA Logo",
+        "title": "Gets DMCA Strikes Removed"
+    },
+    {
+        "clicked": false,
+        "img": clbCover,
+        "alt": "Certified Lover Boy Album Cover",
+        "title": "Finishes Listening to CLB"
+    },
+    {
+        "clicked": false,
+        "img": fourWeirdGym,
+        "alt": "4Weird Emote Photoshopped onto Guy Bro",
+        "title": "Goes to the Gym"
+    },
+    {
+        "clicked": false,
+        "img": goldenKappa,
+        "alt": "Golden Kappa Emote",
+        "title": "Golden Kappa in Chat"
+    },
+    {
+        "clicked": false,
+        "img": viewerIcon,
+        "alt": "Twitch Viewer Icon",
+        "title": "Gets 100k+ Viewers"
+    },
+    {
+        "clicked": false,
+        "img": owTrophies,
+        "alt": "3 Overwatch League Trophies",
+        "title": "Wins 3rd Overwatch League Championship"
+    },
+    {
+        "clicked": false,
+        "img": owRank,
+        "alt": "Overwatch Top 500 Icon",
+        "title": "Rank 1 Support"
+    },
+    {
+        "clicked": false,
+        "img": reinImage,
+        "alt": "ReinHardt Shatter",
+        "title": "6 Man Shatter"
+    },
+    {
+        "clicked": false,
+        "img": superCloseUp,
+        "alt": "Close Up of Super's Face",
+        "title": "Puts on Chapstick"
+    },
+    {
+        "clicked": false,
+        "img": genshinCover,
+        "alt": "Genshin Impact Cover",
+        "title": "Plays Genshin Impact"
+    },
+    {
+        "clicked": false,
+        "img": superHairCut,
+        "alt": "Super with a Haircut",
+        "title": "Gets a Good Haircut"
+    },
+    {
+        "clicked": false,
+        "img": ow2Logo,
+        "alt": "Overwatch 2 Logo",
+        "title": "Plays the Release of Overwatch 2"
+    },
+    {
+        "clicked": false,
+        "img": tifa,
+        "alt": "Tifa",
+        "title": "Tifa Cosplay"
+    },
+    {
+        "clicked": false,
+        "img": superCashew,
+        "alt": "A Cashew with Super's Face on it",
+        "title": "Subreddit Becomes Actually Good"
+    },
+    {
+        "clicked": false,
+        "img": sevenTV,
+        "alt": "7TV Logo",
+        "title": "Installs 7TV"
+    },
+    {
+        "clicked": false,
+        "img": DIESOFCRINGE,
+        "alt": "DIESOFCRINGE Emote",
+        "title": "Adds DIESOFCRINGE"
+    },
+    {
+        "clicked": false,
+        "img": fran,
+        "alt": "Photo of Fran",
+        "title": "Plays a Game with Fran without Arguing"
+    },
+    {
+        "clicked": false,
+        "img": superBeard,
+        "alt": "Super with a Full Beard",
+        "title": "Grows a FULL Beard"
+    },
+    {
+        "clicked": false,
+        "img": owRank,
+        "alt": "Overwatch Top 500 Icon",
+        "title": "Hits 4.8k Rank"
+    },
+    {
+        "clicked": false,
+        "img": superTier3,
+        "alt": "Super's Tier 3 Emote",
+        "title": "New Tier 3 Emote"
+    },
+    {
+        "clicked": false,
+        "img": doomfist,
+        "alt": "Doomfist",
+        "title": "Pops off on Doomfist"
+    },
+    {
+        "clicked": false,
+        "img": NaM,
+        "alt": "NaM Emote",
+        "title": "Figures Out What a NaM Game is"
+    },
+    {
+        "clicked": false,
+        "img": persona5,
+        "alt": "Persona 5 Cover",
+        "title": "Plays Persona 5"
+    },
+    {
+        "clicked": false,
+        "img": owQueue,
+        "alt": "Overwatch Queue Banner",
+        "title": "Starts Stream and IMMEDIATELY Queues"
+    },
+    {
+        "clicked": false,
+        "img": suprL,
+        "alt": "suprL Emote",
+        "title": "Has a Bad Stream"
+    },
+]
